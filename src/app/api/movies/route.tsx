404d3904f394ff334645dbdb481467f214495e04
Derek Martens <martensderek@gmail.com>
2025-03-16 16:25:12 -0500

    Log api error to console

--- src/app/api/movies/route.tsx
+++ src/app/api/movies/route.tsx
@@ -14,6 +14,7 @@ export async function GET(req: Request) {
         const data = await response.json();
         return Response.json(data);
     } catch (error) {
+        console.error(error);
         return Response.json({ error: "Failed to fetch data" }, { status: 500 });
     }
 }
